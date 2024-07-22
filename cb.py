5863a19db7da375acc1c7f659a09b285ad699242
Taina Coleman <tainagdcoleman@gmail.com>
2024-07-22 14:52:42 -0700

    Adds pre-processing script

--- cb.py
+++ cb.py
@@ -16,7 +16,8 @@ def main():
 
     # five different combos of hardware with N_FEATURES for mean
     N_HARDWARE = 2 #(#cpu, mem) - (2, 16), (2, 8), (3, 24)
-    N_FEATURES = 1 # inputs from the workflow, runtime
+    N_FEATURES = 5 # inputs from the workflow, runtime
+    N_WORKFLOWS = 30
     N_ROUNDS = 100
 
     # Setting up the decaying epsilon
@@ -24,22 +25,34 @@ def main():
     e_decay = 0.9
     e_min = 0.0
 
+    ALL_WORKFLOW_FEATURES = {i: np.random.random(N_FEATURES) for i in range(N_WORKFLOWS)}
+    get_workflow_features = lambda workflow_idx: ALL_WORKFLOW_FEATURES[workflow_idx]
+
     # This is made up data - should be replaced with real data to be the ground truth
-    noise_mean, noise_std = np.random.random(N_HARDWARE) * 5, np.random.random(N_HARDWARE) * 0.1
-    coef_truth = np.random.random(size=(N_HARDWARE,N_FEATURES)) * 4 - 2
+    noise_mean, noise_std = np.random.random(N_HARDWARE) * 5, np.random.random(N_HARDWARE) * 0.1 # get this from the data too
+    coef_truth = np.random.random(size=(N_HARDWARE,N_FEATURES)) * 4 - 2 # this is supposed to be the ground truth 
 
-    sample_runtime = lambda hardware_idx, workflow_features: (
-        np.dot(coef_truth[hardware_idx], workflow_features) + 
+    get_runtime = lambda hardware_idx, workflow_idx: (
+        np.dot(coef_truth[hardware_idx], get_workflow_features(workflow_idx)) + 
         np.random.normal(noise_mean[hardware_idx], noise_std[hardware_idx])
-    )
-
+    ) 
     
-    sample_best_runtime = lambda workflow_features: (
+    sample_best_runtime = lambda workflow_idx: (
         np.min([
-            np.dot(coef_truth[hardware_idx], workflow_features) + noise_mean[hardware_idx]
+            np.dot(coef_truth[hardware_idx], get_workflow_features(workflow_idx)) + noise_mean[hardware_idx]
             for hardware_idx in range(N_HARDWARE)
         ])
     )
+
+    # # TODO: Implement to get the features of the workflow from the data
+    # get_workflow_features = lambda workflow_idx: ()
+
+    # # TODO: Implement to get the runtime from the data
+    # get_runtime = lambda hardware_idx, workflow_idx: () 
+
+    # # TODO: Implement to get the best runtime from the data
+    # sample_best_runtime = lambda workflow_idx: ()
+
     # Use e-greedy algo to find the best hardware 
     # samples gathered over time
     # samples = {1: [(workflow, runtime), ...], ...}
@@ -55,25 +68,26 @@ def main():
     rows_runtime = []
     e = e_start
 
-    def get_best_hardware(workflow):
+    def get_best_hardware(workflow_idx):
         return  np.argmin([
-            np.dot(coefs[i], workflow) + noise_coefs[i][0] # predicted runtime
+            np.dot(coefs[i], get_workflow_features(workflow_idx)) + noise_coefs[i][0] # predicted runtime
             for i in range(N_HARDWARE)
         ])
     
     for round_i in range(N_ROUNDS):
         # Made up workflow - should be replaced with real data
-        workflow = np.random.random(N_FEATURES)
+        workflow_idx = np.random.randint(N_WORKFLOWS) # select from the 3 types of instances we have one at random
+        workflow_features = get_workflow_features(workflow_idx) # get the features of the workflow
         if np.random.random() < e:  # explore
             hardware_idx = np.random.randint(N_HARDWARE)
         else:  # exploit
-            hardware_idx = get_best_hardware(workflow)
+            hardware_idx = get_best_hardware(workflow_idx)
 
         # Update the samples
-        actual_runtime = sample_runtime(hardware_idx, workflow)
-        samples[hardware_idx].append((workflow, actual_runtime))
+        actual_runtime = get_runtime(hardware_idx, workflow_idx) # instead of doing this now, we can just look up the runtime for the workflow with the hardware_idx
+        samples[hardware_idx].append((workflow_features, actual_runtime)) 
 
-        best_runtime = sample_best_runtime(workflow)
+        best_runtime = sample_best_runtime(workflow_idx) # get the best runtime using the workflow among all hardwares
         rows_runtime.append({
             "round": round_i,
             "runtime": actual_runtime,
@@ -96,18 +110,27 @@ def main():
         for samples_idx in range(N_HARDWARE):
             for h_idx in range(N_HARDWARE):
                 # Made up workflow - should be replaced with real data
-                workflow = np.random.random(N_FEATURES)
-                predicted_runtime = np.dot(coefs[h_idx], workflow) + noise_coefs[h_idx][0]
-                actual_runtime = sample_runtime(h_idx, workflow)
+                workflow_idx = np.random.randint(N_WORKFLOWS) # select from the 3 types of instances we have one at random
+                workflow_features = get_workflow_features(workflow_idx) # get the features of the workflow
+                predicted_runtime = np.dot(coefs[h_idx], workflow_features) + noise_coefs[h_idx][0]
+                actual_runtime = get_runtime(h_idx, workflow_idx)
                 rows_predictions.append({
                     "round": round_i,
                     "hardware": h_idx,
                     "error": np.abs(predicted_runtime - actual_runtime)
                 })
 
-
-    best_hardware_idx = get_best_hardware([0.5])
-    print(f"Best hardware: {best_hardware_idx}")
+    # Print predicted and actual best hardware for each workflow
+    rows_pred = []
+    for workflow_idx in range(N_WORKFLOWS):
+        best_hardware = get_best_hardware(workflow_idx)
+        actual_best_hardware = np.argmin([
+            get_runtime(hardware_idx, workflow_idx)
+            for hardware_idx in range(N_HARDWARE)
+        ])
+        rows_pred.append([workflow_idx, best_hardware, actual_best_hardware])
+    df_pred = pd.DataFrame(rows_pred, columns=["workflow", "predicted_best_hardware", "actual_best_hardware"])
+    print(df_pred)
 
     df_predictions = pd.DataFrame(rows_predictions)
     # print the hardware with the lowest error over all rounds summed up
---
+++ pre_process.py
@@ -0,0 +1,142 @@
+import pathlib
+import json
+import argparse
+import pandas as pd
+import enum
+import os
+import re
+from typing import Dict
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+class Hardware(enum.Enum):
+    """Characterize hardware by the number of cpus and memory in a tuple (#cpu, mem (MB)), for example: H1 = (1, 16)"""
+
+    H1 = (2, 16)
+    H2 = (3, 24)
+    H3 = (4, 16)  # Add more if needed
+
+    @classmethod
+    def from_spec(cls, cpus, mem):
+        for hardware in cls:
+            if hardware.value == (cpus, mem):
+                return hardware
+        raise ValueError(f"No matching hardware for {cpus} CPUs and {mem} MB memory.")
+    
+def get_hardware(name: str) -> Hardware:
+    """Get hardware values"""
+    try:
+        return Hardware[name.upper()]
+    except KeyError:
+        raise ValueError(f"Invalid hardware name: {name}") 
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Pre-process data for the integrated performance framework")
+    parser.add_argument("--base_path", type=pathlib.Path, required=True, help="Path to the head folder where data is stored.")
+    
+    return parser
+
+def traverse_data(base_path: pathlib.Path) -> pd.DataFrame:
+    """Traverses the results folder and loads all data found in csv files"""
+    # Traverse the directory structure
+    dfs = {}
+    for root, dirs, files in os.walk(base_path):
+        for file in files:
+            if file.endswith('.csv'):
+                # Construct the full file path
+                file_path = os.path.join(root, file)
+                
+                # Extract the parent folder name
+                folder_name = os.path.basename(root)
+                
+                # Read the CSV file into a DataFrame
+                df = load_data(file_path)
+                
+            # Add the DataFrame to the dictionary under the folder name
+            if folder_name not in dfs:
+                dfs[folder_name] = pd.DataFrame()
+            
+            dfs[folder_name] = pd.concat([dfs[folder_name], df], ignore_index=True)
+    
+    return dfs
+
+
+def identify_hardware(data: Dict[str, pd.DataFrame]) -> pd.DataFrame:
+    """Identify the hardware used in the data"""
+    ext_df = pd.DataFrame()
+    for key, value in data.items():
+        # Get the number of cpus and memory
+        pattern = r"(\d+)cores_(\d+)gb"
+        match = re.search(pattern, key)
+
+        if match:
+            cores = match.group(1)
+            gb = match.group(2)
+
+            hardware = Hardware.from_spec(int(cores), int(gb))
+            print(f"Hardware: {hardware.name} = {hardware.value}")
+            
+            # Check if value is a DataFrame
+            if not isinstance(value, pd.DataFrame):
+                print(f"Error: Expected a DataFrame, but got {type(value)} for key {key}")
+                continue
+
+            ext_df = extend_df(value, hardware, ext_df)
+        else:
+            print("Directory did not follow the expected pattern")
+
+    return ext_df
+
+def extend_df(df: pd.DataFrame, hardware: Hardware, ext_df: pd.DataFrame) -> pd.DataFrame:
+    """Extend the dataframe with the hardware information"""
+    
+    df["hardware"] = hardware.name
+    ext_df = pd.concat([ext_df, df], ignore_index=True)
+
+    return ext_df   
+
+
+def load_data(data_path: pathlib.Path) -> pd.DataFrame:
+    """Load data from the data file"""
+    try:
+        # Data in a dataframe already
+        df = pd.read_csv(data_path)
+    except:
+        # Data is in a json file
+        with open(data_path, 'r') as f:
+            info = json.load(f)
+        df = pd.DataFrame(info)
+    
+    return df
+
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Clean data"""
+
+    # Remove unamed column
+    data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
+    
+    return data
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    dfs = traverse_data(pathlib.Path(args.base_path))
+    # print(dfs)
+
+    full_df = identify_hardware(dfs)
+    print(full_df)
+
+    full_df = clean_data(full_df)
+    
+    # Save the data in a csv file 
+    save_dir = this_dir.joinpath("results/data")
+    save_dir.mkdir(parents=True, exist_ok=True)
+    full_df.to_csv(f"{save_dir}/data.csv", index=False)
+ 
+
+if __name__ == "__main__":
+    main()
+
+# base_path = /home/tainagdcoleman/grafana_data_retrieval/data
