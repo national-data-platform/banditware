42e624a136c77e12eb53f99cbedad930f88e1bf2
ravishende <ravi.sameer.shende@gmail.com>
2025-09-29 17:50:09 -0700

    updated comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     data = pd.read_csv("preprocessed_data/matmul_w_time.csv")
     print(data)
     bw = BanditWare(
-        data=data,  # Note: if you are running this after the first run was stopped halfway, comment this line out if you don't want the progress to be lost
+        data=data,  # Note: if you are running this after the first run was stopped halfway, comment this line out if you don't want the previous progress to be lost
         feature_cols=["size", "sparsity", "min", "max"],
         save_dir="mm_time_new",
         model_choice=Model.RANDOM_FOREST,
